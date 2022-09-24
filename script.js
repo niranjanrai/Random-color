88685082fac68475b86f6cf3337367274bb44632
niranjanrai <rainiranjan1998@gmail.com>
2022-09-25 01:28:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const mainButton = document.querySelector("button");
+const body = document.body;
+const currentColor = document.querySelector(".curren-color");
+// body.style.backgroundColor = "blue";
+
+function randomColorGenerator() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const randomColor = `rgb(${red}, ${green}, ${blue})`;
+  return randomColor;
+}
+
+mainButton.addEventListener("click", function () {
+  const randomColor = randomColorGenerator();
+  console.log(randomColor);
+  body.style.backgroundColor = randomColor;
+  currentColor.textContent = randomColor;
+});
